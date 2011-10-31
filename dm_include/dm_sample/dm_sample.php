aae02f968f13ae38b281ac571e4f3830048c50ea
Luke <voxel@mac-mini.local>
2011-10-31 16:30:11 -0400

    initial commit

---
+++ dm_include/dm_sample/dm_sample.php
@@ -0,0 +1,198 @@
+<?php
+
+/**
+ * My Device Module Class
+ *
+ * You will want to change dm_mydevicemodule to something a little more
+ * descriptive, however be sure to retain the dm_ prefix.
+ * 
+ * When complete, place your finished device module into
+ * include/device_modules/
+ * 
+ * @author John Smith <jsmith@ubersmith.com>
+ * @package default
+ **/
+
+class dm_mydevicemodule extends device_module
+{
+	/**
+	 * Device Module Title
+	 *
+	 * This function returns the title of your device module. It will be included
+	 * in the light blue title bar on the Device Manager page for your device,
+	 * as well as in the configuration drop down in the Device Types section of
+	 * 'setup & admin'.
+	 *
+	 * @author John Smith <jsmith@ubersmith.com>
+	 * @return string
+	 **/
+	function title()
+	{
+		return 'My Device Module';
+	}
+	
+	/**
+	 * Device Module Initialization
+	 *
+	 * This function will perform some basic initialization routines. In this 
+	 * example, custom data for the device is retrieved. Other useful calls
+	 * could be included in this step, if needed.
+	 * 
+	 * @return void
+	 * @author John Smith <jsmith@ubersmith.com>
+	 */
+	function init($request = array())
+	{
+		if (isset($this->device)) {
+			$this->metas = device_metadata($this->device);
+		}
+	}
+	
+	/**
+	 * Device Module Summary
+	 *
+	 * This function returns a string (usually of HTML) that will be displayed
+	 * in the Device Module's 'box' on the device's Device Manager page
+	 * 
+	 * This can be used to display retrieved information, or provide a link
+	 * to a utility outside of Ubersmith, or to call some functionality within
+	 * Ubersmith.
+	 * 
+	 * @author John Smith <jsmith@ubersmith.com>
+	 * @return string
+	 **/
+	function summary($request = array())
+	{
+		/**
+		 * These two lines request the metadata for the device, which can
+		 * be useful if you need to make an external request or some other
+		 * decision based on the information associated with the device.
+		 */
+		$this->init($request);
+		$metas  =& $this->metas;
+		$device =& $this->device;
+		/**
+		 * Use this->config('variable') to retrieve values set in 
+		 * 'setup & admin', if necessary.
+		 */
+		$username = $this->config('username');
+		
+		$output = '<b>My Device Module</b><br />';
+		$output .= 'Today is ' . date("D M j") . '</br>';
+		$output .= 'Configured username is: ' . $username. '<br />';
+		
+		foreach ($device as $key => $value) {
+			$output .= $key . ' = ' . $value . '<br />';
+		}
+		
+		foreach ($metas as $key => $value) {
+			$output .= $key . ' = ' . $value . '<br />';
+		}
+		
+		return $output;
+	}
+	
+	/**
+	 * Device Module Configuration Items
+	 *
+	 * This function returns an array of configuration options that will be 
+	 * displayed when the module is configured in the Device Types section of 
+	 * setup & admin.
+	 *
+	 * @author John Smith <jsmith@ubersmith.com>
+	 * @return array
+	 **/
+	function config_items()
+	{
+		return array(
+			'username' => array(
+				'label' => uber_i18n('Username:'),
+				'type' => 'text',
+				'size' => 30,
+				'default' => '',
+			),
+			'decision' => array(
+				'label'   => uber_i18n('Make a decision:'),
+				'type'    => 'select',
+				'options' => array(
+					'0' => 'No',
+					'1' => 'Yes',
+				),
+				'default' => '0',
+			),
+		);
+	}
+
+	/**
+	 * Device Module Monitor Function
+	 *
+	 * This function allows you to include a monitor for your device that
+	 * will trigger based on some output provided by your module, or some
+	 * other calculation.
+	 * 
+	 * This function returns a PEAR::raiseError object on error, otherwise
+	 * returns true, indicating a successful run of the monitor.
+	 * 
+	 * This function is optional.
+	 * 
+	 * @author John Smith <jsmith@ubersmith.com>
+	 * @return mixed
+	 **/
+	function monitor($monitor)
+	{
+		/**
+		 * This code creates a bogus monitor that will probably fail most of the time.
+		 */
+		$value = rand(0, $monitor['extra']['min']) + 5;
+		
+		if (isset($monitor['extra']['min']) && strlen($monitor['extra']['min']) > 0) {
+			if ($value < $monitor['extra']['min']) {
+				return PEAR::raiseError(sprintf(uber_i18n('Value (%1$s) is less than minimum (%2$s)'),$value,$monitor['extra']['min']),1);
+			}
+		}
+		
+		if (isset($monitor['extra']['max']) && strlen($monitor['extra']['max']) > 0) {
+			if ($value > $monitor['extra']['max']) {
+				return PEAR::raiseError(sprintf(uber_i18n('Value (%1$s) is greater than maximum (%2$s)'),$value,$monitor['extra']['max']),1);
+			}
+			
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Device Module Monitor Configuration Items
+	 *
+	 * If your device module is going to have a monitor, this function will return
+	 * an array of the possible configuration options. For example, if you want
+	 * to check if a value returned by your module is between two other values,
+	 * you could set a minimum and maximum value, and use them in the 
+	 * monitor() code above. 
+	 *
+	 * This function is optional.
+	 * 
+	 * @author John Smith <jsmith@ubersmith.com>
+	 * @return array
+	 **/
+	function monitor_config_items()
+	{
+		return array(
+			'min' => array(
+				'label' => uber_i18n('Minimum:'),
+				'type' => 'text',
+				'size' => 10,
+				'default' => '',
+			),
+			'max' => array(
+				'label' => uber_i18n('Maximum:'),
+				'type' => 'text',
+				'size' => 10,
+				'default' => '',
+			),
+		);
+	}
+
+}
+
+// end of script
---
+++ readme.md
@@ -0,0 +1,7 @@
+ubersmith device_modules
+========================
+
+
+This is a skeleton repo to collect user-submitted device modules.
+
+Include your device module & files in the dm_include directory.
